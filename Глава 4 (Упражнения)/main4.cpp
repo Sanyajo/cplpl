8e25bea4b4ac1cd4f11c5456c4f62602277b9b71
Sanyajo <sakhatskiy2016@mail.ru>
2023-03-04 10:47:29 +0300

    new solution

--- Глава 4 (Упражнения)/main4.cpp
+++ Глава 4 (Упражнения)/main4.cpp
@@ -1,135 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
+int main()
+{
+    int lower = 1;
+    int upper = 100;
+    int middle;
 
-int ymenh(int a, vector <int> & vec) {
-        string wybor;
-        cout << "Ваше число больше " << (a - (a / 2)) << " ?";
-        cin >> wybor;
+    char answer;
 
-        if (wybor == "y") {
-            vec.erase(vec.begin(), vec.end() - (vec.size() / 2));
+    while( lower < upper )
+    {
+        middle = (upper + lower) / 2;
 
-            for (int i: vec) {
-                cout << i << " ";
-            }
-
-            if(vec.size() <=3){
-                for(int i: vec){
-                    string vybor;cout<<"\nВаше число: "<<i;
-                    cin>>vybor;
-                    if(vybor == "y"){cout<<"\nЧисло: "<<i<<endl;break;}
-                }
-            }
-            else{
-                return ymenh((a + (a / 2)), vec);
-            }
-
+        do
+        {
+            cout << "Ваше число меньше или равно " << middle << "? (y/n)";
+            cin >> answer;
         }
-        else {
-            vec.erase(vec.begin() + (vec.size() / 2), vec.end());
+        while( answer != 'y' && answer != 'n' );
 
-            for (int i: vec) {
-                cout << i << " ";
-            }
+        if( answer == 'y' )
+            upper = middle;
+        else
+            lower = middle + 1;
 
-            if(vec.size() <=3){
-                for(int i: vec){
-                    string vybor;cout<<"\nВаше число: "<<i;
-                cin>>vybor;
-                    if(vybor == "y"){cout<<"\nЧисло: "<<i<<endl;break;}
-                }
-            }
-            else{
-                return ymenh((a - (a / 2)), vec);
-            }
-
-        }
-}
-
-void ywel(int a, vector <int> & vec) {
-    string wybor;
-    a=(a + (a / 2));
-    if(a>=100){a=100;}
-    cout << "Ваше число больше " << a << " ?";
-    cin >> wybor;
-    if (wybor == "y") {
-        vec.erase(vec.begin(), vec.end()-(vec.size()/2));
-
-        for (int i: vec) {
-            cout << i << " ";
-        }
-
-        if(vec.size() <=3){
-            for(int i: vec){
-                string vybor;cout<<"\nВаше число: "<<i;
-                cin>>vybor;
-                if(vybor == "y"){cout<<"\nЧисло: "<<i<<endl;break;}
-            }
-        }
-        else{
-            return ywel(a, vec);
-        }
-
-    }
-    else {
-        vec.erase(vec.begin() + (vec.size() / 2), vec.end());
-        a=(a - (a / 2));
-        for (int i: vec) {
-            cout << i << " ";
-        }
-
-        if(vec.size() <=3){
-            for(int i: vec){
-                string vybor;cout<<"\nВаше число: "<<i;
-                cin>>vybor;
-                if(vybor == "y"){cout<<"\nЧисло: "<<i<<endl;break;}
-            }
-        }
-        else{
-            return ywel(a, vec);
-        }
-
-    }
-}
-
-void inputVec(vector <int>& vec1, vector <int>& vec2){
-    int i=1;
-    while ( i<=50){
-        vec1.push_back(i);
-        vec2.push_back(50+i);
-        i++;
+        cout << "[" << lower << " : " << upper << "]\n";
     }
 
-}
-
-int main(){
-    vector <int> vec1;
-    vector <int> vec2;
-    inputVec(vec1,vec2);
-
-    int number;
-    cout<<"Загадайте число от 1 до 100: ";
-    cin>>number;
-
-    string vybor;
-    cout<<"Число больше 50? ";
-    cin>>vybor;
-
-
-    if(vybor == "y")    ywel(49,vec2);
-    else    ymenh(50,vec1);
-
-
-//    for(int i: vec1){
-//        cout<<i<<" ";
-//    }
-//
-//    cout<<endl<<endl;
-//    for( int i: vec2){
-//        cout<<i<<" ";
-//    }
+    cout << "Ваше число  " << upper << "\n";
 
     return 0;
 }
