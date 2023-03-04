48541b832fecbe34e7dd91f25135a78192c34f4e
Sanyajo <sakhatskiy2016@mail.ru>
2023-03-04 11:00:14 +0300

    new solver

--- Глава 4 (Упражнения)/main6.cpp
+++ Глава 4 (Упражнения)/main6.cpp
@@ -8,16 +8,26 @@ int main(){
     int num;
     cout<<"Введите число: ";
     cin>>num;
-    cout<<"Число как строка: "<<vec[num]<<'\n';
+    if(num > 9)     {cout<<"Ввели недопустимое значение!!!"; return 1;}
+    else    cout<<"Число как строка: "<<vec[num]<<'\n';
 
     string str;
     cout<<"Введите строку: ";
     cin>>str;
 
+    int k = 0;
     for(int i=0;i<vec.size();++i){
-        if(str == vec[i]){
-            cout<<"Строка как число: "<<i;
+        if(str == vec[i]){k++;}
+    }
+
+    if(k == 0){cout<<"Ввели недопустимое значение!!!"; return 1;}
+    else{
+        for(int i=0;i<vec.size();++i){
+            if(str == vec[i]){
+                cout<<"Строка как число: "<<i;
+            }
         }
     }
+
     return 0;
 }
