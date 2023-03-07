3588591125db0845b21b7bdccc0736b47c19ce2c
Sanyajo <sakhatskiy2016@mail.ru>
2023-03-07 12:39:50 +0300

    update (+-)

--- Глава 4 (Упражнения)/main7.cpp
+++ Глава 4 (Упражнения)/main7.cpp
@@ -17,33 +17,36 @@ bool transformation(bool& check,const string& str, int& x,const vector<string>& 
     }
 }
 
-void Calc(char ch, int x1, int x2){
+void Calc(char ch, int x1, int x2) {
 
     switch (ch) {
-        case '+':
-            cout<<"Сумма "<<x1<<" и "<<x2<<" равна "<<(x1+x2)<<"\n";
+        case '+': {
+            cout << "Сумма " << x1 << " и " << x2 << " равна " << (x1 + x2) << "\n";
             break;
-        case '-':
-        {
+        }
+        case '-': {
             string wybor;
-            cout<<"Уменьшаемое x1?";
-            cin>>wybor;
-            if(wybor == "y")    cout<<"Разность "<<x1<<" и "<<x2<<" равна "<<(x1-x2)<<"\n";
-            else    cout<<"Разность "<<x1<<" и "<<x2<<" равна "<<(x2-x1)<<"\n";
+            cout << "Уменьшаемое x1?";
+            cin >> wybor;
+            if (wybor == "y") cout << "Разность " << x1 << " и " << x2 << " равна " << (x1 - x2) << "\n";
+            else cout << "Разность " << x1 << " и " << x2 << " равна " << (x2 - x1) << "\n";
             break;
         }
 
-        case '/':
-        {
+        case '/': {
             string wybor;
-            cout<<"Делимое x1?";
-            cin>>wybor;
-            if(wybor == "y")    cout<<"Деление "<<x1<<" и "<<x2<<" равно "<<(x1/x2)<<"\n";
-            else    cout<<"Деление "<<x1<<" и "<<x2<<" равно "<<(x2/x1)<<"\n";
+            cout << "Делимое x1?";
+            cin >> wybor;
+            if (wybor == "y") cout << "Деление " << x1 << " и " << x2 << " равно " << (x1 / x2) << "\n";
+            else cout << "Деление " << x1 << " и " << x2 << " равно " << (x2 / x1) << "\n";
             break;
         }
-        case '*':
-            cout<<"Умножение "<<x1<<" и "<<x2<<" равно "<<(x1*x2)<<"\n";
+        case '*': {
+            cout << "Умножение " << x1 << " и " << x2 << " равно " << (x1 * x2) << "\n";
+            break;
+        }
+        default:
+            cout << "Некорректное действие";
             break;
     }
 }
