eec3961ebf0bd7999a1e465fcb89e40a52ea3acd
Sanyajo <sakhatskiy2016@mail.ru>
2023-03-17 01:15:40 +0300

    NEW

--- Глава 4 (Упражнения)/main6.cpp
+++ Глава 4 (Упражнения)/main6.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 int main(){
     vector <string> vec ={"zero","one","two","three","four","five","six","seven","eight","nine"};
@@ -8,8 +7,14 @@ int main(){
     int num;
     cout<<"Введите число: ";
     cin>>num;
-    if(num > 9)     {cout<<"Ввели недопустимое значение!!!"; return 1;}
-    else    cout<<"Число как строка: "<<vec[num]<<'\n';
+    
+    if(num > 9){
+        cout<<"Ввели недопустимое значение!!!";
+        return 1;
+    }
+    else{
+        cout<<"Число как строка: "<<vec[num]<<'\n';
+    }
 
     string str;
     cout<<"Введите строку: ";
@@ -17,16 +22,15 @@ int main(){
 
     bool check = false;
     for(int i=0;i<vec.size();++i){
-        if(str == vec[i]){check = true;}
+        if(str == vec[i]){
+            check = true;
+            cout<<"Строка как число: "<<i;
+        }
     }
 
-    if(!check){cout<<"Ввели недопустимое значение!!!"; return 1;}
-    else{
-        for(int i=0;i<vec.size();++i){
-            if(str == vec[i]){
-                cout<<"Строка как число: "<<i;
-            }
-        }
+    if(!check){
+        cout<<"Ввели недопустимое значение!!!";
+        return 1;
     }
 
     return 0;
