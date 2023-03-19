8f7e095785a2dc37fe4bd279ed5d93cf6a7f3ba3
Sanyajo <sakhatskiy2016@mail.ru>
2023-03-20 00:57:56 +0300

    new_solution (+-)

---
+++ Глава 5 (Упражнения)/main8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    int number = 0;
+    int sum = 0;
+
+    int N = 0;
+    std::cout<<"Input N: ";
+    std::cin>>N;
+
+    std::vector <int> vec;
+
+    while(std::cin>>number){
+        vec.push_back(number);
+    }
+
+    if(N > vec.size()){
+        std::cerr<<"N > vec.size !";
+        return 1;
+    }else{
+        for(int i=0; i <= N-1; ++i){
+            sum += vec[i];
+        }
+
+        std::cout<<sum;
+
+        return 0;
+        }
+}
