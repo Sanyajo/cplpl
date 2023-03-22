30eef7ff898a2906c1285896fa851c445b316c7d
Sanyajo <sakhatskiy2016@mail.ru>
2023-03-22 19:11:08 +0300

    new_solution

--- Глава 5 (Упражнения)/main8.cpp
+++ Глава 5 (Упражнения)/main8.cpp
@@ -1,43 +1,66 @@
 #include <iostream>
 #include <vector>
 
+void inputVec(std::vector <int>& vec){
+    //int i = std::numeric_limits<int>::max();
+    std::string number;
+    while(std::cin>>number) {
+        if (number == "|") {
+            break;
+        }else if(stoi(number) == 0) { //!stoi(number)
+            throw ("Значение не может быть представленно как int ! \n");
+        }else{
+            int n = stoi(number);
+            vec.push_back(n);
+            }
+    }
+}
+
 int main(){
     setlocale(LC_ALL,"RUS");
 
-    int number = 0;
+    std::vector <int> vec;
     int sum = 0;
 
     int N = 0;
     std::cout<<"Введите количество суммируемых значений: \n";
     std::cin>>N;
 
-    if(N < 0){
-        std::cerr<<"N < 0 !";
+    if(N < 0 || std::cin.fail()){
+        std::cerr<<"Invalid N !";
         return 1;
     }
 
-    std::vector <int> vec;
-
     std::cout<<"Введите несколько целых чисел ( | для окончания ввода) :\n";
-
-    while(std::cin>>number){
-        vec.push_back(number);
+    try{
+        inputVec(vec);
+    }catch(const char* msg){
+        std::cerr<<msg;
     }
 
     if(N > vec.size()){
         std::cerr<<"N > vec.size !";
         return 1;
-    }else{
-        for(int i = 0; i <= N-1; ++i){
+    }else {
+        for (int i = 0; i <= N - 1; ++i) {
             sum += vec[i];
         }
+    }
 
+    if(N == 1){
+        std::cout<<"Сумма "<<N<<" числа ( ";
+        for(int i = 0; i <= N-1; ++i){
+            std::cout<<vec[i]<<" ";
+        }
+        std::cout<<") равна "<<sum;
+    }
+    else{
         std::cout<<"Сумма первых "<<N<<" чисел ( ";
         for(int i = 0; i <= N-1; ++i){
             std::cout<<vec[i]<<" ";
         }
         std::cout<<") равна "<<sum;
+    }
 
-        return 0;
-        }
+    return 0;
 }
