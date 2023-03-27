4cc7896c78899cd425f4b036cf3fa31870d455df
Sanyajo <sakhatskiy2016@mail.ru>
2023-03-27 22:34:21 +0300

    new_solution

--- Глава 5 (Упражнения)/main2.cpp
+++ Глава 5 (Упражнения)/main2.cpp
@@ -2,11 +2,20 @@
 
 double ctok (double c){
     double k = c+ 273.15;
-    return k;
+    if (k == 0){
+        throw std::invalid_argument("k == 0");
+    }else{
+        return k;
+    }
 }
 int main(){
     double c = 0;
     std::cin>>c;
-    double k = ctok(c);
-    std::cout<<k;
+    try{
+         double k = ctok(c);
+             std::cout<<k;
+    }catch(const std::invalid_argument& e){
+        std::cerr<<e.what()<<"\n";
+    }
+    return 0;
 }
